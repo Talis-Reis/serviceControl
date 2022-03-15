cc1766cbd439cd2f486b54d7659a11e196365dcd
Talis Reis <talisapreis@gmail.com>
2022-03-15 13:02:03 -0700

    conexao com o banco e primeiras rotas

--- src/server.js
+++ src/server.js
@@ -1,14 +1,34 @@
 const express = require("express");
+const { json } = require("express/lib/response");
+const { Sequelize } = require('sequelize');
 const app = express();
-const port = 8086;
+const port = 8086;  
+const bodyParser = require('body-parser');
 
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get("/", (req,res)=>{
-    res.json("OK");
+    res.json({mesage: "OK"});
 });
 
-app.listen(port, ()=>{
-    console.log(`Servidor iniciado na porta ${port}`);
+app.post("/cadastro", (req, res) =>{
+    const teste = req.body.homeOffice;
+    res.json({HomeOffice: teste});
 })
 
+//conexão com o banco de dados
+const sequelize = new Sequelize('taskcalender', 'talisreis', '@bcard123', {
+    host: '192.168.254.8',
+    dialect: 'mysql'
+});
 
+sequelize.authenticate().then(()=>{
+    console.log('Conexão com o banco de dados. STATUS: SUCESSO!');
+}).catch((erro)=>{
+    console.log('Conexão com o banco de dados. STATUS: FALHA');
+    console.log(`FALHA: ${erro}`);
+})
+
+app.listen(port, ()=> {console.log(`Servidor conectado na porta ${port}`)});
